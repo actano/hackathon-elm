1d1a1c2b1acffe87e3ed0cacd50debc5dfb011c8
Peter Lüderitz <peter.luederitz@yellowman.de>
2017-05-30 18:08:43 +0200

    get user home id

--- rplan.elm
+++ rplan.elm
@@ -12,9 +12,12 @@ type alias Model =
     { username: String
     , password: String
     , errorMessage: String
-    , accessToken: Maybe String
+    , accessToken: Maybe AccessToken
+    , userHomeId: Maybe UserHomeId
     }
 type alias Setter = Model -> String -> Model
+type alias AccessToken = String
+type alias UserHomeId = String
 
 setUsername : Setter
 setUsername model value =
@@ -26,16 +29,28 @@ setPassword model value =
 
 type Msg = SetField Setter String
          | Login
-         | LoginResult (Result Http.Error String)
+         | LoginResult (Result Http.Error AccessToken)
+         | SetUserHomeId (Result Http.Error UserHomeId)
 
 init : ( Model, Cmd Msg )
 init = ( { username = ""
          , password = ""
          , errorMessage = ""
          , accessToken = Nothing
+         , userHomeId = Nothing
          }
        , Cmd.none
        )
+
+setError : Http.Error -> Model -> ( Model, Cmd Msg )
+setError err model =
+  case err of
+    BadUrl errMsg -> ( { model | errorMessage = ("bad url: " ++ errMsg )}, Cmd.none )
+    Timeout -> ( { model | errorMessage = "timeout"}, Cmd.none )
+    NetworkError -> ( { model | errorMessage = "network error"}, Cmd.none )
+    BadStatus response -> ( { model | errorMessage = ("bad status: " ++ (toString response.status) ++ ", " ++ response.body) }, Cmd.none )
+    BadPayload dbgMsg response ->  ( { model | errorMessage = ("bad payload: " ++ dbgMsg ++ ", " ++ (toString response.status) ++ ", " ++ response.body) }, Cmd.none )
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -46,14 +61,12 @@ update msg model =
             (model, (login model.username model.password))
         LoginResult result ->
             case result of
-                Ok token -> ( { model | accessToken = (Just token) }, Cmd.none )
-                Err err ->
-                  case err of
-                    BadUrl errMsg -> ( { model | errorMessage = ("bad url: " ++ errMsg )}, Cmd.none )
-                    Timeout -> ( { model | errorMessage = "timeout"}, Cmd.none )
-                    NetworkError -> ( { model | errorMessage = "network error"}, Cmd.none )
-                    BadStatus response -> ( { model | errorMessage = ("bad status: " ++ (toString response.status) ++ ", " ++ response.body) }, Cmd.none )
-                    BadPayload dbgMsg response ->  ( { model | errorMessage = ("bad payload: " ++ dbgMsg ++ ", " ++ (toString response.status) ++ ", " ++ response.body) }, Cmd.none )
+                Ok token -> ( { model | accessToken = (Just token) }, getUserHome token )
+                Err err -> setError err model
+        SetUserHomeId result ->
+            case result of
+                Ok userHomeId -> ( { model | userHomeId = (Just userHomeId) }, Cmd.none )
+                Err err -> setError err model
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
@@ -66,6 +79,22 @@ login username password =
         (stringBody "application/json" ("{ \"grant_type\": \"password\", \"username\": \"" ++ username ++ "\" , \"password\": \"" ++ password ++ "\"}"))
         (field "access_token" string)
         |> Http.send LoginResult
+
+getUserHome : AccessToken -> Cmd Msg
+getUserHome accessToken =
+    Http.request
+     { method = "GET"
+     , headers = [ (Http.header "Cookie" ("language=de; SID=" ++ accessToken))
+                 , (Http.header "Authorization" ("Bearer " ++ accessToken))
+                 ]
+     , url = "http://localhost:8081/api/user/current/root"
+     , body = Http.emptyBody
+     , expect = Http.expectJson (field "rootPlanningObjectId" string)
+     , timeout = Nothing
+     , withCredentials = False
+     }
+        |> Http.send SetUserHomeId
+
 
 main : Program Never Model Msg
 main =
@@ -86,7 +115,9 @@ renderLoginOrPlaceholder model =
     let
         nodeList = case model.accessToken of
             Just _ ->
-                [ text "You are logged in" ]
+                [ text "You are logged in"
+                , div [] [ text (toString model.userHomeId) ]
+                ]
             Nothing ->
                 [ text "Login"
                 , input [placeholder "Username", value model.username, onInput (SetField setUsername)] []
